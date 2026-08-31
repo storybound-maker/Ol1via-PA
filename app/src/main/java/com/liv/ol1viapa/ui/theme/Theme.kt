c394afbbd49aff8fbf1295cedf495e10cb090791
storybound-maker <storybound622@gmail.com>
2026-08-31 01:26:02 +0000

    Rename Compose theme to LeauPATheme

--- app/src/main/java/com/liv/ol1viapa/ui/theme/Theme.kt
+++ app/src/main/java/com/liv/ol1viapa/ui/theme/Theme.kt
@@ -10,20 +10,11 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-)
+private val DarkColorScheme = darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
+private val LightColorScheme = lightColorScheme(primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40)
 
 @Composable
-fun Ol1viaPATheme(
+fun LeauPATheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -36,10 +27,5 @@ fun Ol1viaPATheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Ol1viaTypography,
-        content = content
-    )
+    MaterialTheme(colorScheme = colorScheme, typography = LeauTypography, content = content)
 }
