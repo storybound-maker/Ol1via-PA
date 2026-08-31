7453e2021e8e540b81565881e3dd86c6fb2f7bb6
storybound-maker <storybound622@gmail.com>
2026-08-31 04:23:55 +0000

    Set Leau visual palette

--- app/src/main/java/com/liv/ol1viapa/ui/theme/Color.kt
+++ app/src/main/java/com/liv/ol1viapa/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.liv.ol1viapa.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LeauLime = Color(0xFFB8FF5A)
+val LeauMint = Color(0xFF69F0C4)
+val LeauGlow = Color(0xFFA3FFD6)
+val LeauBackground = Color(0xFF07110E)
+val LeauSurface = Color(0xFF0D1B17)
+val LeauSurfaceVariant = Color(0xFF17332A)
+val LeauText = Color(0xFFE9FFF5)
+val LeauMuted = Color(0xFF89A89D)
