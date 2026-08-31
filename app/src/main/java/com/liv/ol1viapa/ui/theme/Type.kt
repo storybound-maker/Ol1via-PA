45425e103572f8b2969e3a5275c139c43e0224ad
storybound-maker <storybound622@gmail.com>
2026-08-31 01:29:18 +0000

    Finish Leau typography rename

--- app/src/main/java/com/liv/ol1viapa/ui/theme/Type.kt
+++ app/src/main/java/com/liv/ol1viapa/ui/theme/Type.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-val Ol1viaTypography = Typography(
+val LeauTypography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
